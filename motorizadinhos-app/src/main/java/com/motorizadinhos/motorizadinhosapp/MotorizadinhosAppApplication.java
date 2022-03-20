c58105f612ca284e95f74c8726915eaf6d9878db
igorhenss <igorhenss@gmail.com>
2022-03-19 22:02:15 -0300

    #T003-2 - Criado projeto back-end.

---
+++ motorizadinhos-app/src/main/java/com/motorizadinhos/motorizadinhosapp/MotorizadinhosAppApplication.java
@@ -0,0 +1,13 @@
+package com.motorizadinhos.motorizadinhosapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MotorizadinhosAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MotorizadinhosAppApplication.class, args);
+	}
+
+}
